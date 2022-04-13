4b7615f651440dd30381c4ee09b293aebbaf7936
Rudo2204 <rudo2204@gmail.com>
2022-04-13 16:56:49 +0700

    Add utils

---
+++ lib-utils.js
@@ -0,0 +1,95 @@
+import { getNetworkNodes, canHack, getThresholds, getAvailableRam } from "./utils.js";
+
+/** @param {NS} ns */
+export function getServersToHack(ns) {
+	const cracks = {
+		"BruteSSH.exe": ns.brutessh,
+		"FTPCrack.exe": ns.ftpcrack,
+		"relaySMTP.exe": ns.relaysmtp,
+		"HTTPWorm.exe": ns.httpworm,
+		"SQLInject.exe": ns.sqlinject
+	};
+	var nodes = getNetworkNodes(ns);
+	nodes = nodes.filter(node =>
+		node == "home"
+		&& !node.includes("myserv")
+		&& canHack(ns, node, cracks)
+	);
+	return nodes;
+}
+
+/** @param {NS} ns */
+export function getScriptableServersSorted(ns, sortByMaxRam = true) {
+	const cracks = {
+		"BruteSSH.exe": ns.brutessh,
+		"FTPCrack.exe": ns.ftpcrack,
+		"relaySMTP.exe": ns.relaysmtp,
+		"HTTPWorm.exe": ns.httpworm,
+		"SQLInject.exe": ns.sqlinject
+	};
+	var nodes = getNetworkNodes(ns);
+	nodes = nodes.filter(node =>
+		canHack(ns, node, cracks)
+		&& ns.getServerMaxRam(node) >= 1.5
+	);
+
+	if (sortByMaxRam) {
+		nodes = nodes.sort(function (a, b) { return ns.getServerMaxRam(b) - ns.getServerMaxRam(a) });
+	} else {
+		nodes = nodes.sort(function (a, b) { return getAvailableRam(ns, b) - getAvailableRam(ns, a) });
+	}
+
+	return nodes;
+}
+
+/** @param {NS} ns **/
+export function getAvailableThreads(ns, isHackScript) {
+	const cracks = {
+		"BruteSSH.exe": ns.brutessh,
+		"FTPCrack.exe": ns.ftpcrack,
+		"relaySMTP.exe": ns.relaysmtp,
+		"HTTPWorm.exe": ns.httpworm,
+		"SQLInject.exe": ns.sqlinject
+	};
+
+	const ramScript = isHackScript ? 1.6 : 1.75;
+	var nodes = getNetworkNodes(ns);
+	nodes = nodes.filter(node => ns.getServerMaxRam(node) > 0 && canHack(ns, node, cracks));
+
+	var threadCount = 0;
+
+	for (var i = 0; i < nodes.length; i++) {
+		const node = nodes[i];
+		const maxThreads = parseInt(Math.floor(ns.getServerMaxRam(node) / ramScript));
+
+		threadCount += maxThreads;
+	}
+
+	return threadCount;
+}
+
+/** @param {NS} ns **/
+export function getPrepStrategy(ns, host, target) {
+	const securityDifference = ns.getServerSecurityLevel(target) - ns.getServerMinSecurityLevel(target);
+	const growthMoneyMultiplier = ns.getServerMaxMoney(target) / ns.getServerMoneyAvailable(target);
+	ns.tprint(ns.getServerMaxMoney(target));
+	ns.tprint(growthMoneyMultiplier);
+	if (securityDifference == 0 && growthMoneyMultiplier == 1) {
+		ns.tprint(target, " is prepped!")
+	}
+	const coreCounts = ns.getServer(host).cpuCores;
+
+	const growthThreads = Math.ceil(ns.growthAnalyze(target, growthMoneyMultiplier, coreCounts));
+	const securityAfterGrowth = growthThreads * 0.004 + ns.getServerSecurityLevel(target);
+	const weakenThreads = Math.ceil((securityAfterGrowth - ns.getServerMinSecurityLevel(target)) / 0.05);
+
+	const t0 = 150; // 150ms delay between G and W
+	const grow_time = ns.getGrowTime(target);
+	const weaken_time = ns.getWeakenTime(target);
+	const grow_delay = weaken_time - grow_time + t0;
+	return {
+		grow_delay,
+		growthThreads,
+		weakenThreads,
+	}
+}
---
+++ utils.js
@@ -0,0 +1,87 @@
+const homeServer = "home";
+
+/** @param {NS} ns */
+export function canHack(ns, host, cracks) {
+	if (ns.hasRootAccess(host)) {
+		return true;
+	}
+
+	const playerHackingLevel = ns.getHackingLevel();
+	const serverHackRequiredLevel = ns.getServerRequiredHackingLevel(host);
+
+	const requiredPorts = ns.getServerNumPortsRequired(host);
+	var myCracksCount = 0;
+	for (const file of Object.keys(cracks)) {
+		if (ns.fileExists(file, homeServer)) {
+			myCracksCount += 1;
+		}
+	}
+	return (playerHackingLevel >= serverHackRequiredLevel && myCracksCount >= requiredPorts);
+}
+
+/** @param {NS} ns **/
+export function penetrate(ns, host, cracks) {
+	ns.print("Penetrating " + host);
+	for (const file of Object.keys(cracks)) {
+		if (ns.fileExists(file, homeServer)) {
+			const runScript = cracks[file];
+			runScript(host);
+		}
+	}
+}
+
+/** @param {NS} ns **/
+export function getRootAccess(ns, host, cracks) {
+	const requiredPorts = ns.getServerNumPortsRequired(host);
+	if (requiredPorts > 0) {
+		penetrate(ns, host, cracks);
+	}
+	ns.print("Gaining root access on " + host);
+	ns.nuke(host);
+}
+
+/** @param {NS} ns **/
+export function getNetworkNodes(ns) {
+	var visited = {};
+	var stack = [];
+	const origin = ns.getHostname(homeServer);
+	stack.push(origin);
+
+	while (stack.length > 0) {
+		const node = stack.pop();
+		if (!visited[node]) {
+			visited[node] = node;
+			const neighbours = ns.scan(node);
+			for (var i = 0; i < neighbours.length; i++) {
+				const child = neighbours[i];
+				if (visited[child]) {
+					continue;
+				}
+				stack.push(child);
+			}
+		}
+	}
+
+	// we only want the keys, as the value is the as the key
+	return Object.keys(visited);
+}
+
+/** @param {NS} ns **/
+export async function copyAndRunHackScript(ns, host, hackScript, target, cracks) {
+	const hackScriptRam = ns.getScriptRam(hackScript);
+	if (!ns.isRunning(hackScript, host, target) && canHack(ns, host, cracks)) {
+		const maxThreads = parseInt(Math.floor(ns.getServerMaxRam(host) / hackScriptRam));
+		if (maxThreads == 0) {
+			ns.print(host, " has too little ram, skipping...");
+			return;
+		}
+		ns.print("Copying hackScript to server: " + host);
+		await ns.scp(hackScript, host);
+		ns.scriptKill(hackScript, host);
+		ns.exec(hackScript, host, maxThreads, target);
+	}
+}
+
+export function getAvailableRam(ns, server) {
+	return ns.getServerMaxRam(server) - ns.getServerUsedRam(server);
+}
