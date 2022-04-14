c2036bc8f27da6acf369bedb6c26af40e8b3b444
Rudo2204 <rudo2204@gmail.com>
2022-04-14 18:37:15 +0700

    Add manual batch attack

--- batch-attack.js
+++ batch-attack.js
@@ -1,21 +1,38 @@
 import { calculateDelays, calculateThreads } from "./batch-math.js";
 /** @param {NS} ns */
 export async function main(ns) {
-	const hackScript = "hack-target.js";
-	const weakenScript = "weaken-target.js";
-	const growScript = "grow-target.js";
+	const cracks = {
+		"BruteSSH.exe": ns.brutessh,
+		"FTPCrack.exe": ns.ftpcrack,
+		"relaySMTP.exe": ns.relaysmtp,
+		"HTTPWorm.exe": ns.httpworm,
+		"SQLInject.exe": ns.sqlinject
+	};
+
+	const hackScript = "hack.js";
+	const weakenScript = "weaken.js";
+	const growScript = "grow.js";
 
 	const target = ns.args[0];
-	//const target = "phantasy";
 	const host = ns.args[1]; // "home";
 	const percentHack = ns.args[2];
+
+    if (ns.args.length < 3) {
+        ns.tprint("Not enough args. 0 -> target, 1 -> host, 2 -> percent");
+        ns.exit();
+    }
+
+    if (!ns.hasRootAccess(target) && canHack(ns, target, cracks)) {
+        getRootAccess(ns, host, cracks);
+    }
 
 	var {
 		period,
 		hack_delay,
 		weak_delay_1,
 		grow_delay,
-		weak_delay_2
+		weak_delay_2,
+		depth
 	} = calculateDelays(ns, target);
 
 	var {
@@ -29,28 +46,21 @@ export async function main(ns) {
 	const weaken1Ram = weakenThreads_1 * ns.getScriptRam(weakenScript);
 	const growthRam = growthThreads * ns.getScriptRam(growScript);
 	const weaken2Ram = weakenThreads_2 * ns.getScriptRam(weakenScript);
-
-	ns.tprint("target: ", target, ", host: ", host);
-	const profitPerBatch = ns.getServerMaxMoney(target) * percentHack;
-	ns.tprint("profit/batch = ", profitPerBatch, ". Rate = ", profitPerBatch/(5*50/1000));
-	ns.tprint(hackRam);
-	ns.tprint(weaken1Ram);
-	ns.tprint(growthRam);
-	ns.tprint(weaken2Ram);
-
 	const sumRam = hackRam + weaken1Ram + growthRam + weaken2Ram;
-	ns.tprint("sum = ", sumRam);
 
-	const batch_count = 10;
+	const availableRam = ns.getServerMaxRam(host) - ns.getServerUsedRam(host) - reservedRam;
+	const no_batch = Math.min(availableRam/sumRam, depth);
+
 	const safe_window = 250;
 
-	//for (var i = 0; i < batch_count; i++) {
-	//	ns.exec(hackScript, serv, hackThreads, hack_delay + safe_window + period*i, target);
-	//	ns.exec(weakenScript, serv, weakenThreads_1, weak_delay_1 + safe_window + period*i, target);
-	//	ns.exec(growScript, serv, growthThreads, grow_delay + safe_window + period*i, target);
-	//	ns.exec(weakenScript, serv, weakenThreads_2, weak_delay_2 + safe_window + period*i, target);
-	//}
-	//while (true) {
-	//	await ns.asleep(1000);
-	//}
+    ns.print("Scheduling ", no_batch, " batches on on target: ", target);
+	for (var i = 0; i < no_batch; i++) {
+		ns.exec(hackScript, serv, hackThreads, hack_delay + safe_window + period*i, target);
+		ns.exec(weakenScript, serv, weakenThreads_1, weak_delay_1 + safe_window + period*i, target);
+		ns.exec(growScript, serv, growthThreads, grow_delay + safe_window + period*i, target);
+		ns.exec(weakenScript, serv, weakenThreads_2, weak_delay_2 + safe_window + period*i, target);
+	}
+	while (true) {
+		await ns.asleep(1000);
+	}
 }
